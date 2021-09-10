8e591b3c2606a7f019ffdfc8f7fd32bb3f58147a
Masanin <gmasmin@gmail.com>
2021-09-10 15:03:48 +0300

    saga_fix

--- src/Redux/Sagas/WordsSagas/wordsSagas.ts
+++ src/Redux/Sagas/WordsSagas/wordsSagas.ts
@@ -1,28 +1,32 @@
 import getWords from '../../../Axios/requests'
 import { paths } from '../../../Routes/Routes'
-import { LOCATION_CHANGE } from 'connected-react-router'
+import { LocationChangeAction, LOCATION_CHANGE } from 'connected-react-router'
 import { WordsActionsTypes } from '../../Words/interfaces'
-import { call, fork, put, take, takeEvery } from 'redux-saga/effects'
+import { call, put, takeEvery } from 'redux-saga/effects'
 import { setIsLoadingAction, setWordsAction } from '../../Words/wordsActions'
+import {
+  GetWordsOnRouteEnterResults,
+  GetWordsWorkerResult,
+  WordaSagaResult,
+} from '../Types'
 
-function* getWordsWorker(): any {
+function* getWordsWorker(): GetWordsWorkerResult {
   yield put(setIsLoadingAction(true))
   const words = yield call(getWords)
   yield put(setWordsAction(words))
 }
 
-export function* getWordsOnRouteEnter(): any {
-  while (true) {
-    const action = yield take(LOCATION_CHANGE)
-    if (action.payload.location.pathname === paths.MAIN) {
-      yield put(setIsLoadingAction(true))
-      const words = yield call(getWords)
-      yield put(setWordsAction(words))
-    }
+export function* getWordsOnRouteEnter(
+  action: LocationChangeAction
+): GetWordsOnRouteEnterResults {
+  if (action.payload.location.pathname === paths.MAIN) {
+    yield put(setIsLoadingAction(true))
+    const words = yield call(getWords)
+    yield put(setWordsAction(words))
   }
 }
 
-export function* wordsSaga() {
-  yield fork(getWordsOnRouteEnter)
+export function* wordsSaga(): WordaSagaResult {
   yield takeEvery(WordsActionsTypes.GET_WORDS, getWordsWorker)
+  yield takeEvery(LOCATION_CHANGE, getWordsOnRouteEnter)
 }
